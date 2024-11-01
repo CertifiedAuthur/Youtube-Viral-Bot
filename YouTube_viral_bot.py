1f56ef919ef798844c07223b3a9c79875339ddef
Chibuzor Okafor <134807240+CertifiedAuthur@users.noreply.github.com>
2024-11-01 16:31:35 +0100

    Update YouTube_viral_bot.py

--- YouTube_viral_bot.py
+++ YouTube_viral_bot.py
@@ -376,19 +376,10 @@ def get_trending_keywords(country):
     df["Search Volume"] = df["Search Volume"].apply(format_number)
     return df
 
-# Button to trigger the login process, always displayed
-login_button = st.sidebar.button("Proceed to Google Login")
 
-CLIENT_ID = st.sidebar.text_input("Enter your Client ID")
-CLIENT_SECRET = st.sidebar.text_input("Enter your Client Secret Key")
+CLIENT_ID = st.secrets["CLIENT_ID"]
+CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
 REDIRECT_URI = st.secrets["REDIRECT_URI"]
-
-
-# Button to initiate login
-if st.sidebar.button("Log in with Google"):
-    if CLIENT_ID and CLIENT_SECRET:
-        # Initialize Google OAuth2 client
-        client = GoogleOAuth2(CLIENT_ID, CLIENT_SECRET)
 
 # Display Google login link
 def get_login_str():
